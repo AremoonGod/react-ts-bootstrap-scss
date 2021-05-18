3e5a3aeec10d2a9418a8c185bbaca0f354c472bc
AmreenDev <amreen0094@gmail.com>
2021-05-17 19:39:22 -0700

    create new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import Home from "./Home";
-import Navbar from "./Navbar";
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div>
-        <Home />
-      </div>
+      Hellow World
     </div>
   );
 }
